deec6c821ac6cb1359c595152c3f21ad5572c288
florianlev <34171315+florianlev@users.noreply.github.com>
2018-04-13 16:30:06 -0400

    requete voyagesPourUnVaisseau

--- serveur/src/accesseur/VoyagesDAO.php
+++ serveur/src/accesseur/VoyagesDAO.php
@@ -12,5 +12,13 @@
             return $requeteListerVoyages->fetchAll(PDO::FETCH_OBJ);
         }
 
+        function voyagesPourUnVaisseau($unVaisseau)
+        {
+            $LISTER_VOYAGE = "SELECT * FROM voyage WHERE idVaisseau = $unVaisseau;";
+            global $basededonnees;
+            $requeteListerVoyagesVaisseau = $basededonnees->prepare($LISTER_VOYAGE);
+            $requeteListerVoyagesVaisseau->execute();
+            return $requeteListerVoyagesVaisseau->fetchAll(PDO::FETCH_OBJ);
+        }
     }
 ?>
